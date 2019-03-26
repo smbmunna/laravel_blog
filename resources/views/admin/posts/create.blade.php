23ecd62c8ec6c352c4b437ab9c2befa660250c3d
smbmunna <smbmunna@gmail.com>
2019-03-27 00:25:56 +0600

    Post controller and Post Table added

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/admin');
+
+
+@section('content');
+
+<h1>Create Post</h1>
+
+
+@endsection();
--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,7 @@ Route::group(['middleware'=>'admin'], function(){
 
     Route::resource('admin/users','AdminUsersController');
 
+    Route::resource('admin/posts','AdminPostsController');
+
 
 });
